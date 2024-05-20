79dc4b26121ada05b693f685991fb706faf8f940
AlanZheng2580 <168727160+AlanZheng2580@users.noreply.github.com>
2024-05-20 11:45:01 +0800

    update 20240517002_inset_user_test.sql

--- sql/myfirstdb/20240517002_inset_user_test.sql
+++ sql/myfirstdb/20240517002_inset_user_test.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 ALTER TABLE user_test
-ADD COLUMN email VARCHAR(255) NOT NULL DEFAULT '';
+ADD COLUMN email VARCHAR(255) NOT NULL DEFAULT '' COMMENT '使用者Email，不能為空';
 
 -- +migrate Down
 ALTER TABLE user_test
